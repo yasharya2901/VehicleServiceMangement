f9ea24d1aa0bb7f014419a8310e9da2562bcd8ff
yasharya2901 <yasharya2901@gmail.com>
2024-04-28 21:39:33 +0530

    First Commit: Initialized the Repository

---
+++ src/main/java/com/scaler/groupproject/vehicleservicemangement/VehicleServiceMangementApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.groupproject.vehicleservicemangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceMangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleServiceMangementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/groupproject/vehicleservicemangement/VehicleServiceMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.groupproject.vehicleservicemangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceMangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
