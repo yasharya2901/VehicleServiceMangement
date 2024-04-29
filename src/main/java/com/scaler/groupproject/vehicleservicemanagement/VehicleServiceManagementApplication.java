e9f6a5729a2cff7f2361c73b3ac09ae2e78e88aa
yasharya2901 <yasharya2901@gmail.com>
2024-04-29 17:06:55 +0530

    Fixed the typo: vehicleServiceMangement to vehicleServiceManagement Also added the DrawIO file of database model

--- src/main/java/com/scaler/groupproject/vehicleservicemanagement/VehicleServiceManagementApplication.java
+++ src/main/java/com/scaler/groupproject/vehicleservicemanagement/VehicleServiceManagementApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VehicleServiceMangementApplication {
-
+public class VehicleServiceManagementApplication {
     public static void main(String[] args) {
-        SpringApplication.run(VehicleServiceMangementApplication.class, args);
+        SpringApplication.run(VehicleServiceManagementApplication.class, args);
     }
 
 }
--- src/test/java/com/scaler/groupproject/vehicleservicemangement/VehicleServiceManagementApplicationTests.java
+++ src/test/java/com/scaler/groupproject/vehicleservicemangement/VehicleServiceManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VehicleServiceMangementApplicationTests {
+class VehicleServiceManagementApplicationTests {
 
     @Test
     void contextLoads() {
